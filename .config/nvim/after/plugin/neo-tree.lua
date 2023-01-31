352c2d73cfe25021ac7e1022c49b3fc51fab1e2c
bitterhalt <95308907+bitterhalt@users.noreply.github.com>
2023-01-31 03:21:05 +0200

    edit nvim-tree

--- .config/nvim/after/plugin/neo-tree.lua
+++ .config/nvim/after/plugin/neo-tree.lua
@@ -13,7 +13,6 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 nvim_tree.setup {
     disable_netrw = true,
   hijack_netrw = true,
-   ignore_ft_on_setup = { "alpha" },
   update_focused_file = {
     enable = true,
     update_cwd = true,
