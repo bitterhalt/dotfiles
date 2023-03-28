f4eb3703fd173c392eda715fcb1cfe569ed985d2
bitterhalt <95308907+bitterhalt@users.noreply.github.com>
2023-03-28 20:22:09 +0300

    remove status lines in dashboard..

--- .config/nvim/after/plugin/alpha.lua
+++ .config/nvim/after/plugin/alpha.lua
@@ -37,19 +37,33 @@ dashboard.opts.opts.noautocmd = false
 alpha.setup(dashboard.opts)
 
 
+-- Disable statuslines in dashboard
 
--- Disable statusline in dashboard
+vim.api.nvim_create_autocmd("User", {
+    pattern = "AlphaReady",
+    desc = "disable tabline for alpha",
+    callback = function()
+        vim.opt.showtabline = 0
+    end,
+})
+vim.api.nvim_create_autocmd("BufUnload", {
+    buffer = 0,
+    desc = "enable tabline after alpha",
+    callback = function()
+        vim.opt.showtabline = 2
+    end,
+})
+
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "alpha",
-  callback = function()
-    -- store current statusline value and use that
-    local old_laststatus = vim.opt.laststatus
-    vim.api.nvim_create_autocmd("BufUnload", {
-      buffer = 0,
-      callback = function()
-        vim.opt.laststatus = old_laststatus
-      end,
-    })
-    vim.opt.laststatus = 0
-  end,
+    pattern = "alpha",
+    callback = function()
+        local old_laststatus = vim.opt.laststatus
+        vim.api.nvim_create_autocmd("BufUnload", {
+            buffer = 0,
+            callback = function()
+                vim.opt.laststatus = old_laststatus
+            end,
+        })
+        vim.opt.laststatus = 0
+    end,
 })
