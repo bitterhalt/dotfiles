8733b58d705edfbf1e0b890c696b6116eb360944
bitterhalt <95308907+bitterhalt@users.noreply.github.com>
2023-08-18 00:14:27 +0300

    fix neo-tree: https://github.com/nvim-tree/nvim-tree.lua/wiki/Migrating-To-on_attach

--- .config/nvim/after/plugin/neo-tree.lua
+++ .config/nvim/after/plugin/neo-tree.lua
@@ -1,17 +1,17 @@
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if not status_ok then
-  return
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-nvim_tree.setup {
-    disable_netrw = true,
+require("nvim-tree").setup {
+  on_attach = my_on_attach,
+  disable_netrw = true,
   hijack_netrw = true,
   update_focused_file = {
     enable = true,
--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -32,6 +32,7 @@ return packer.startup(function(use)
     use ('wbthomason/packer.nvim' )
     use ('folke/which-key.nvim')
     use ('nvim-lualine/lualine.nvim')
+   -- use {'nvim-tree/nvim-tree.lua', commit = 'ace64228ad5d89035fbe6f85e7f45a1f7b9e29c1' }
     use ('nvim-tree/nvim-tree.lua')
     use ('kyazdani42/nvim-web-devicons')
     use ('nvim-treesitter/nvim-treesitter')
