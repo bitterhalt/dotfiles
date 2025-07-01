3de5dc39ec9ad7112f32cdb9f575b7c03fe6097a
bitterhalt <bthalt@disroot.org>
2025-07-01 12:36:10 +0300

    cleanup

--- home/bazz/.config/waybar/scripts/mic.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-if wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -qi 'MUTED'; then
-  echo ""
-else
-  echo "{\"text\": \"󰍬\", \"tooltip\": \"<b>Microphone is active</b>\n click left or press  + F10 to disable\"}"
-fi
