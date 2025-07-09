9c6039d8037e0896cc7aeacf82694bade7744a93
bitterhalt <bthalt@disroot.org>
2025-07-09 14:34:18 +0300

    arch transformation

--- home/bazz/.config/imv/copy_img.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-wl-copy -t image/png <"$1"
-notify-send \
-  -i "$(realpath $1)" \
-  "${1%.*}" \
-  "Copied to clipboard"
--- home/bazz/.config/sway/scripts/autotiling-toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if pgrep autotiling >/dev/null; then
-  pkill autotiling
-  notify-send -i ~/.local/share/icons/feather/x.svg "Autotiling" "Disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
-else
-  autotiling -l 2 &
-  notify-send -i ~/.local/share/icons/feather/check.svg "Autotiling" "Enabled" -t 1500 -h string:x-canonical-private-synchronous:volume
-fi
