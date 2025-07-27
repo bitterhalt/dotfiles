f2e341728ea91ed2781fa7a41d502e01fd131b68
bitterhalt <bthalt@disroot.org>
2025-07-27 22:08:35 +0300

    add hypridle and hyprlock

--- home/bazz/.config/hypr/scripts/dot-settings.sh
+++ home/bazz/.config/hypr/scripts/dot-settings.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 # Menu options
-ARCHIVE="HYPRLAND
-FOOT
+ARCHIVE="BASH
 FNOTT
+FOOT
 FUZZEL
-WAYBAR
-BASH
+HYPRIDLE
+HYPRLAND
+HYPRLOCK
 NEOVIM
+WAYBAR
 "
-CHOICE=$(printf "$ARCHIVE" | fuzzel -d -a top --y 4 -w 30 -l 7 -p "Edit settings: ")
+CHOICE=$(printf "$ARCHIVE" | fuzzel -d -a top --y 4 -w 30 -l 9 -p "" --placeholder "Edit settings: ")
 
 TERMCMD=("foot")
 
@@ -18,7 +20,9 @@ BASH) $TERMCMD -e nvim $HOME/.bashrc ;;
 FNOTT) $TERMCMD -e nvim $HOME/.config/fnott/fnott.ini ;;
 FOOT) $TERMCMD -e nvim $HOME/.config/foot/foot.ini ;;
 FUZZEL) $TERMCMD -e nvim $HOME/.config/fuzzel/fuzzel.ini ;;
-NEOVIM) $TERMCMD -e nvim $HOME/.config/nvim/init.lua ;;
+HYPRIDLE) $TERMCMD -e nvim $HOME/.config/hypr/hypridle.conf ;;
 HYPRLAND) $TERMCMD -e nvim $HOME/.config/hypr/hyprland.conf ;;
+HYPRLOCK) $TERMCMD -e nvim $HOME/.config/hypr/hyprlock.conf ;;
+NEOVIM) $TERMCMD -e nvim $HOME/.config/nvim/init.lua ;;
 WAYBAR) $TERMCMD -e nvim -p $HOME/.config/waybar/config.jsonc $HOME/.config/waybar/style.css ;;
 esac
