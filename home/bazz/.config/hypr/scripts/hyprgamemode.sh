3bd6995124c7abb76c05bf69e1e5fefc85960bbb
bitterhalt <bthalt@disroot.org>
2025-08-03 03:39:53 +0300

    add small notification

--- home/bazz/.config/hypr/scripts/hyprgamemode.sh
+++ home/bazz/.config/hypr/scripts/hyprgamemode.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env sh
+
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+
 if [ "$HYPRGAMEMODE" = 1 ]; then
   hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:shadow:enabled 0;\
         keyword decoration:blur:enabled 0"
+  notify-send -t 5000 "Hyprland" "Fancy stuff disabled"
   exit
+else
+  hyprctl reload
+  notify-send -t 5000 "Hyprland" "Fancy stuff enabled"
 fi
-hyprctl reload
