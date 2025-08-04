cc0b6765fea3be85bf086dd7abc5e979f2b265fd
bitterhalt <bthalt@disroot.org>
2025-08-04 12:31:44 +0300

    bar stuff

--- home/bazz/.config/sway/waybar/scripts/waybar-hider.sh
+++ home/bazz/.config/sway/waybar/scripts/waybar-hider.sh
@@ -7,5 +7,5 @@ if pgrep -x waybar >/dev/null; then
   notify-send -t 2000 -a System -i ~/.local/share/icons/feather/x.svg "Waybar" "disabled" -h string:x-canonical-private-synchronous:volume
 else
   # Launch Waybar
-  waybar &
+  waybar -c ~/.config/sway/waybar/config-sway.jsonc -s ~/.config/sway/waybar/style-sway.css &
 fi
--- home/bazz/.config/waybar/scripts/idle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if pgrep -x "swayidle" >/dev/null; then
-  echo ""
-else
-  echo "{\"text\": \"󰒳\", \"tooltip\": \"<b>Idle timer is disabled</b>\n click left or press  +Control+F11 to enable\"}"
-fi
