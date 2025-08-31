721eeda24157f089ce1ffad9c58d6f4112f6b30f
bitterhalt <bthalt@disroot.org>
2025-08-31 20:22:14 +0300

    edit split ratio

--- home/bazz/.config/sway/scripts/autotiling-toggle.sh
+++ home/bazz/.config/sway/scripts/autotiling-toggle.sh
@@ -4,6 +4,6 @@ if pgrep autotiling >/dev/null; then
   pkill autotiling
   notify-send -i ~/.local/share/icons/feather/x.svg "Autotiling" "Disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
 else
-  autotiling -l 2 &
+  autotiling -l 2 -sw 0.95 &
   notify-send -i ~/.local/share/icons/feather/check.svg "Autotiling" "Enabled" -t 1500 -h string:x-canonical-private-synchronous:volume
 fi
