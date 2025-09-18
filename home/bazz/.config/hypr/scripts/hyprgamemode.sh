d70d0fc48ba84181504ac3efb5b4dee3feea5f25
bitterhalt <bthalt@disroot.org>
2025-09-18 14:34:51 +0300

    stuff

--- home/bazz/.config/hypr/scripts/hyprgamemode.sh
+++ home/bazz/.config/hypr/scripts/hyprgamemode.sh
@@ -6,7 +6,8 @@ if [ "$HYPRGAMEMODE" = 1 ]; then
   hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:shadow:enabled 0;\
-        keyword decoration:blur:enabled 0"
+        keyword decoration:blur:enabled 0; \
+        keyword decoration:rounding 0"
   notify-send -t 5000 "Hyprland" "Fancy stuff disabled"
   exit
 else
