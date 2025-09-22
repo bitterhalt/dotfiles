8c653149ac18b2bb642a8d87be02dd632f718fcd
bitterhalt <bthalt@disroot.org>
2025-09-22 10:20:22 +0300

    add bufferline

--- home/bazz/.config/hypr/scripts/dot-settings.sh
+++ home/bazz/.config/hypr/scripts/dot-settings.sh
@@ -5,13 +5,11 @@ ARCHIVE="BASH
 FNOTT
 FOOT
 FUZZEL
-HYPRIDLE
 HYPRLAND
-HYPRLOCK
 NEOVIM
 WAYBAR
 "
-CHOICE=$(printf "$ARCHIVE" | fuzzel -d -a top --y 4 -w 30 -l 9 -p "" --placeholder "Edit settings: ")
+CHOICE=$(printf "$ARCHIVE" | fuzzel -d --minimal-lines -a top --y 4 -w 30 -p "" --placeholder "Edit settings: ")
 
 TERMCMD=("foot")
 
@@ -20,9 +18,7 @@ BASH) $TERMCMD -e nvim $HOME/.bashrc ;;
 FNOTT) $TERMCMD -e nvim $HOME/.config/fnott/fnott.ini ;;
 FOOT) $TERMCMD -e nvim $HOME/.config/foot/foot.ini ;;
 FUZZEL) $TERMCMD -e nvim $HOME/.config/fuzzel/fuzzel.ini ;;
-HYPRIDLE) $TERMCMD -e nvim $HOME/.config/hypr/hypridle.conf ;;
-HYPRLAND) $TERMCMD -e nvim $HOME/.config/hypr/hyprland.conf ;;
-HYPRLOCK) $TERMCMD -e nvim $HOME/.config/hypr/hyprlock.conf ;;
+HYPRLAND) $TERMCMD -e nvim $HOME/.config/hypr/hyprland.conf $HOME/.config/hypr/hyprlock.conf $HOME/.config/hypr/hypridle.conf ;;
 NEOVIM) $TERMCMD -e nvim $HOME/.config/nvim/init.lua ;;
-WAYBAR) $TERMCMD -e nvim $HOME/.config/hypr/waybar/config.jsonc ;;
+WAYBAR) $TERMCMD -e nvim $HOME/.config/hypr/waybar/config.jsonc $HOME/.config/hypr/waybar/style.css ;;
 esac
--- home/bazz/.config/nvim/lua/plugins/ui.lua
+++ home/bazz/.config/nvim/lua/plugins/ui.lua
@@ -9,6 +9,43 @@ return {
     end,
   },
 
+  --Bufferline
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "famiu/bufdelete.nvim",
+    },
+    opts = {
+      options = {
+        numbers = "none",
+        middle_mouse_command = nil,
+        indicator_icon = nil,
+        indicator = { style = "none" },
+        buffer_close_icon = "x",
+        modified_icon = "*",
+        close_icon = "x",
+        left_trunc_marker = "<",
+        right_trunc_marker = ">",
+        max_name_length = 30,
+        max_prefix_length = 30,
+        tab_size = 21,
+        diagnostics = false, -- | "nvim_lsp" | "coc",
+        diagnostics_update_in_insert = false,
+        show_buffer_icons = true,
+        show_buffer_close_icons = true,
+        show_close_icon = true,
+        show_tab_indicators = true,
+        persist_buffer_sort = true,
+        separator_style = "thin",
+        enforce_regular_tabs = true,
+        always_show_bufferline = true,
+      },
+    },
+  },
+
   -- Lualine
   {
     "nvim-lualine/lualine.nvim",
