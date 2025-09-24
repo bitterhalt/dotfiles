c99a9d1d0fea045f0ff884ca3e8c8ce3d4a78365
bitterhalt <bthalt@disroot.org>
2025-09-24 11:34:38 +0300

    sway sfutt

--- home/bazz/.config/sway/scripts/dot-settings.sh
+++ home/bazz/.config/sway/scripts/dot-settings.sh
@@ -15,12 +15,12 @@ CHOICE=$(printf "$ARCHIVE" | fuzzel -d -a top --y 4 -w 30 -l 8 -p "Edit settings
 TERMCMD=("foot")
 
 case $CHOICE in
-BASH) $TERMCMD -e nvim $HOME/.bashrc ;;
-FNOTT) $TERMCMD -e nvim $HOME/.config/fnott/fnott.ini ;;
-FOOT) $TERMCMD -e nvim $HOME/.config/foot/foot.ini ;;
-FUZZEL) $TERMCMD -e nvim $HOME/.config/fuzzel/fuzzel.ini ;;
-NEOVIM) $TERMCMD -e nvim $HOME/.config/nvim/init.lua ;;
-SWAY) $TERMCMD -e nvim $HOME/.config/sway/config ;;
-SWAYIDLE) $TERMCMD -e nvim $HOME/.config/swayidle/config ;;
-WAYBAR) $TERMCMD -e nvim $HOME/.config/sway/waybar/config.jsonc ;;
+BASH) exec $TERMCMD -e nvim $HOME/.bashrc ;;
+FNOTT) exec $TERMCMD -e nvim $HOME/.config/fnott/fnott.ini ;;
+FOOT) exec $TERMCMD -e nvim $HOME/.config/foot/foot.ini ;;
+FUZZEL) exec $TERMCMD -e nvim $HOME/.config/fuzzel/fuzzel.ini ;;
+NEOVIM) exec $TERMCMD -e nvim $HOME/.config/nvim/init.lua ;;
+SWAY) exec $TERMCMD -e nvim $HOME/.config/sway/config ;;
+SWAYIDLE) exec $TERMCMD -e nvim $HOME/.config/swayidle/config ;;
+WAYBAR) exec $TERMCMD -e nvim $HOME/.config/sway/waybar/config.jsonc ;;
 esac
---
+++ home/bazz/.config/sway/waybar.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+CFG="$HOME/.config/sway/waybar"
+
+start() {
+  pidof waybar || waybar -c "$CFG/config.jsonc" -s "$CFG/style.css" &
+}
+
+#!/usr/bin/env bash
+set -e
+CFG="$HOME/.config/sway/waybar"
+
+start() {
+  if ! pgrep -x waybar >/dev/null; then
+    waybar -c "$CFG/config.jsonc" -s "$CFG/style.css" &
+  fi
+}
+
+toggle() {
+  if pgrep -x waybar >/dev/null; then
+    killall -q waybar
+    notify-send -t 2000 -a System \
+      -i ~/.local/share/icons/feather/x.svg \
+      "Waybar" "disabled" \
+      -h string:x-canonical-private-synchronous:volume
+  else
+    waybar -c "$CFG/config.jsonc" -s "$CFG/style.css" &
+  fi
+}
+
+case "$1" in
+start) start ;;
+toggle) toggle ;;
+*)
+  echo "Usage: $0 {start|toggle}" >&2
+  exit 1
+  ;;
+esac
