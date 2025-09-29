dbe157491236fdad4e15b6c6d8c25366270a6fbf
bitterhalt <bthalt@disroot.org>
2025-09-29 18:11:57 +0300

    stuff for sway

---
+++ home/bazz/.config/sway/scripts/idle-daemon.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+locker="hyprlock"
+icons="$HOME/.local/share/icons/feather/"
+
+toggle() {
+  if pgrep swayidle >/dev/null; then
+    pkill swayidle
+    notify-send -i "$icons"/eye.svg "Swayidle" "Disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
+  else
+    notify-send -i "$icons"/eye-off.svg "Swayidle" "Enabled" -t 1500 -h string:x-canonical-private-synchronous:volume
+    swayidle -w \
+      timeout 1700 "pidof $locker >/dev/null || $locker &" \
+      timeout 1800 "swaymsg 'output * power off'" resume "swaymsg 'output * power on'" \
+      before-sleep "$locker" &
+  fi
+}
+
+start() {
+  swayidle -w \
+    timeout 1700 "pidof $locker >/dev/null || $locker &" \
+    timeout 1800 "swaymsg 'output * power off'" resume "swaymsg 'output * power on'" \
+    before-sleep "$locker" &
+}
+
+case "$1" in
+"--toggle")
+  toggle
+  ;;
+*)
+  start
+  ;;
+esac
