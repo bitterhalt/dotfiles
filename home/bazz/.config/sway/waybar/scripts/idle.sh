718e6723f49780062a0050fcc2e9c8ab21a39148
bitterhalt <bthalt@disroot.org>
2025-10-10 08:51:49 +0300

    power is yellow :p

--- home/bazz/.config/sway/waybar/scripts/idle.sh
+++ home/bazz/.config/sway/waybar/scripts/idle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if pgrep -x "swayidle" >/dev/null; then
-  echo "{\"text\": \"󰤄\", \"tooltip\": \"<b>Swayidle is enabled</b>\n click left or press  + Shift + F11 to disable\"}"
+  echo "{\"text\": \"󰤄\", \"tooltip\": \"<b>swayidle is enabled</b>\n click left or press  + Shift + F11 to disable\", \"class\": \"enabled\"}"
 else
-  echo "{\"text\": \"󰠠\", \"tooltip\": \"<b>Swayidle is disabled</b>\n click left or press  + Shift + F11 to enable\"}"
+  echo "{\"text\": \"󰠠\", \"tooltip\": \"<b>swayidle is disabled</b>\n click left or press  + Shift + F11 to enable\", \"class\": \"disabled\"}"
 fi
