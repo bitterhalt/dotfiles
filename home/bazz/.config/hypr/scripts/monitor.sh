e19033da090152eda5205b73f33816e2ce4fb912
bitterhalt <bthalt@disroot.org>
2025-10-12 18:05:54 +0300

    minor edits

--- home/bazz/.config/hypr/scripts/monitor.sh
+++ home/bazz/.config/hypr/scripts/monitor.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# Sleep to make sure all start correct order
+wait="sleep 0.2"
+
+# Launch some monitor stuff in worksspace 1
+# Make sure that class "syspace" rule is addet in Hyprland rules
+
 foot -a syspace -T journalctl -e journalctl -f &
-sleep 0.2
+$wait
 foot -a syspace -T htop -e htop &
-sleep 0.2
+$wait
 foot -a syspace -T atop -e amdgpu_top --smi &
