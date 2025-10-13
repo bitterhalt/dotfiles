997a66342db11201bbc7ab38c7dea2edb88b4dfe
bitterhalt <bthalt@disroot.org>
2025-10-13 11:24:50 +0300

    open colors too

--- home/bazz/.config/hypr/scripts/dot-settings.sh
+++ home/bazz/.config/hypr/scripts/dot-settings.sh
@@ -21,7 +21,7 @@ FNOTT) exec $TERMCMD -e nvim $HOME/.config/fnott/fnott.ini ;;
 FOOT) exec $TERMCMD -e nvim $HOME/.config/foot/foot.ini ;;
 FUZZEL) exec $TERMCMD -e nvim $HOME/.config/fuzzel/fuzzel.ini ;;
 HYPRIDLE) exec $TERMCMD -e nvim $HOME/.config/hypr/hypridle.conf ;;
-HYPRLAND) exec $TERMCMD -e nvim $HOME/.config/hypr/modules/* ;;
+HYPRLAND) exec $TERMCMD -e nvim $HOME/.config/hypr/modules/* $HOME/.config/hypr/colors.conf ;;
 HYPRLOCK) exec $TERMCMD -e nvim $HOME/.config/hypr/hyprlock.conf ;;
 NEOVIM) exec $TERMCMD -e nvim $HOME/.config/nvim/init.lua ;;
 WAYBAR) exec $TERMCMD -e nvim $HOME/.config/hypr/waybar/config.jsonc $HOME/.config/hypr/waybar/style.css ;;
