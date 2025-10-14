c5c839efe9b651ddf27e0785944197451cb31526
bitterhalt <bthalt@disroot.org>
2025-10-14 15:19:41 +0300

    make more consistent and remove tray

--- home/bazz/.config/hypr/waybar/scripts/idle.sh
+++ home/bazz/.config/hypr/waybar/scripts/idle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if pgrep -x "hypridle" >/dev/null; then
-  echo "{\"text\": \"󰤄\", \"tooltip\": \"<b>Hypridle is enabled</b>\n click left or press  + Shift + F11 to disable\", \"class\": \"enabled\"}"
+  echo "{\"text\": \"󰤄\", \"tooltip\": \"<b>Hypridle is enabled</b>\nClick left to disable\nClick right to Power-menu\",\"class\": \"enabled\"}"
 else
-  echo "{\"text\": \"󰠠\", \"tooltip\": \"<b>Hypridle is disabled</b>\n click left or press  + Shift + F11 to enable\", \"class\": \"disabled\"}"
+  echo "{\"text\": \"󰠠\", \"tooltip\": \"<b>Hypridle is disable</b>\nClick left to enable\nClick right to Power-menu\", \"class\": \"disabled\"}"
 fi
