213e6409a924a22ea03a8354dc22638a05e2c1c6
bitterhalt <bthalt@disroot.org>
2025-10-24 02:49:09 +0300

    Run barless by default just for fun small edits for notifications

---
+++ home/bazz/.config/hypr/scripts/welcome.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Show welcome popup only if Waybar is NOT running
+
+if ! pgrep -x "waybar" >/dev/null; then
+  user=$(whoami)
+  day=$(date '+%A, %d %B')
+  clock=$(date '+%H:%M')
+
+  body=$(printf "%s\n%s\n\n%s" \
+    "Today is $day" \
+    "Time is $clock" \
+    "Tip: press SUPER+B to enable bar")
+
+  notify-send -t 15000 "Welcome back $user ❤️" "\n$body"
+fi
