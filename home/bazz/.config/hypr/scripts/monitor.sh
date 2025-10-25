6d35144b7fba058f16a96d666f7a51d7f40534a4
bitterhalt <bthalt@disroot.org>
2025-10-25 12:10:49 +0300

    ..

--- home/bazz/.config/hypr/scripts/monitor.sh
+++ home/bazz/.config/hypr/scripts/monitor.sh
@@ -2,8 +2,8 @@
 
 wait="sleep 0.2"
 
-foot -a syspace -T journalctl -e journalctl -f &
-$wait
 foot -a syspace -T htop -e htop &
 $wait
 foot -a syspace -T atop -e amdgpu_top --smi &
+$wait
+foot -a syspace -T journalctl -e journalctl -f &
