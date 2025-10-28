f211bd2df49708483722ab03d438b22810cd8026
bitterhalt <bthalt@disroot.org>
2025-10-28 16:42:00 +0200

    consistensy

--- home/bazz/.local/bin/tmux-sysmon.sh
+++ home/bazz/.local/bin/tmux-sysmon.sh
@@ -5,7 +5,7 @@ SESSION="system"
 # If session already exists, just attach to it
 if tmux has-session -t "$SESSION" 2>/dev/null; then
   notify-send -t 5000 "TMUX" "Re-attaching to existing session: $SESSION"
-  exec tmux new-session -As "$SESSION"
+  exec tmux attach-session -t "$SESSION"
 fi
 
 tmux new-session -d -s "$SESSION" -n logs "journalctl -f"
