6b5b9aa71c60a5a0550cebfbcef1b1309cba2c9f
bitterhalt <bthalt@disroot.org>
2025-10-29 22:48:30 +0200

    edit gamemode and some rules

--- home/bazz/.config/hypr/scripts/hyprgamemode.sh
+++ home/bazz/.config/hypr/scripts/hyprgamemode.sh
@@ -1,16 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 
 if [ "$HYPRGAMEMODE" = 1 ]; then
   hyprctl --batch "\
         keyword animations:enabled 0;\
+        keyword animation borderangle,0; \
         keyword decoration:shadow:enabled 0;\
-        keyword decoration:blur:enabled 0; \
+        keyword decoration:blur:enabled 0;\
+	      keyword decoration:fullscreen_opacity 1;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
         keyword decoration:rounding 0"
   notify-send -t 5000 "Hyprland" "Fancy stuff disabled"
-  exit
+  exit 0
 else
   hyprctl reload
   notify-send -t 5000 "Hyprland" "Fancy stuff enabled"
+  exit 0
 fi
