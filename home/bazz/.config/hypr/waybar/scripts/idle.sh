83eed6550287b6504bfc787d923f1454eda6cc5d
bitterhalt <bthalt@disroot.org>
2025-10-30 17:20:01 +0200

    add empty line

--- home/bazz/.config/hypr/waybar/scripts/idle.sh
+++ home/bazz/.config/hypr/waybar/scripts/idle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if pgrep -x "hypridle" >/dev/null; then
-  echo "{\"text\": \"󰤄\", \"tooltip\": \"<b>Hypridle is enabled</b>\nClick left to disable\nClick right to Power-menu\",\"class\": \"enabled\"}"
+  echo "{\"text\": \"󰤄\", \"tooltip\": \"<b>Hypridle is enabled</b>\n\nClick left to disable\nClick right to Power-menu\",\"class\": \"enabled\"}"
 else
-  echo "{\"text\": \"󰠠\", \"tooltip\": \"<b>Hypridle is disable</b>\nClick left to enable\nClick right to Power-menu\", \"class\": \"disabled\"}"
+  echo "{\"text\": \"󰠠\", \"tooltip\": \"<b>Hypridle is disable</b>\n\nClick left to enable\nClick right to Power-menu\", \"class\": \"disabled\"}"
 fi
