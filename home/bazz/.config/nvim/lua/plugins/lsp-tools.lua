f0cc6240717dd3508e1917817eb0bbcca2991c20
bitterhalt <bthalt@disroot.org>
2025-11-09 17:25:03 +0200

    Rewrite messy task timer with Python and tidy neovim config

---
+++ home/bazz/.config/nvim/lua/plugins/lsp-tools.lua
@@ -0,0 +1,273 @@
+return {
+  -- Mason.nvim
+  {
+    "williamboman/mason.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      local mason = require("mason")
+      local mason_tool_installer = require("mason-tool-installer")
+
+      mason.setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+      mason_tool_installer.setup({
+        ensure_installed = {
+          "prettier", -- prettier formatter
+          "stylua", -- lua formatter
+          "isort", -- python formatter
+          "black", -- python formatter
+          "eslint_d", -- js linter
+          "shfmt", -- Shell formatter
+          "shellcheck",
+        },
+      })
+    end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = true,
+    opts = {
+      ensure_installed = {
+        "ruff",
+        "html",
+        "lua_ls",
+        "ts_ls",
+        "bashls",
+      },
+      handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup({})
+        end,
+
+        ["html"] = function()
+          require("lspconfig").html.setup({
+            filetypes = { "html", "htmldjango" },
+          })
+        end,
+
+        ["lua_ls"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.lua_ls.setup({
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
+                },
+              },
+            },
+          })
+        end,
+      },
+    },
+  },
+
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.completion.spell,
+        },
+      })
+    end,
+    lazy = true,
+  },
+
+  -- Conform (Formatter)
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    config = function()
+      local conform = require("conform")
+
+      conform.setup({
+        formatters_by_ft = {
+           "prettier",
+          "stylua",
+          "isort",
+          "black",
+          "ruff",
+          "eslint_d",
+          "shfmt",
+          "shellcheck",
+          -- LSP servers
+          "html",
+          "lua_ls",
+          "tsserver",
+          "bashls",
+        },
+        format_on_save = {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        },
+      })
+
+      vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+        conform.format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        })
+      end, { desc = "Format file or range (in visual mode)" })
+    end,
+  },
+
+  -- Nvim-lint
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+        svelte = { "eslint_d" },
+        python = { "ruff" },
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>li", function()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
+    end,
+  },
+
+  -- nvim-lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        callback = function()
+          vim.lsp.buf.format()
+        end,
+      })
+    end,
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    keys = {
+      {
+        "K",
+        vim.lsp.buf.hover,
+        desc = "Display information about the symbol under the cursor",
+      },
+      {
+        "gd",
+        vim.lsp.buf.definition,
+        desc = "Go to definition",
+      },
+      {
+        "crr",
+        vim.lsp.buf.code_action,
+        mode = { "n", "v" },
+        desc = "List available code actions",
+      },
+      {
+        "crn",
+        vim.lsp.buf.rename,
+        desc = "Renames all references to the symbol under the cursor",
+      },
+      {
+        "<leader>=",
+        vim.lsp.buf.format,
+        desc = "Formats a buffer using the attached language server",
+      },
+    },
+  },
+
+  -- nvim-cmp
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer", -- text from currrent buffer
+      "hrsh7th/cmp-path", -- file system paths
+      "hrsh7th/cmp-nvim-lsp", --  nvim language servers
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
+      },
+      "rafamadriz/friendly-snippets",
+      "saadparwaiz1/cmp_luasnip",
+      "onsails/lspkind.nvim",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
+        snippet = { -- configure how nvim-cmp interacts with snippet engine
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered({
+            winhighlight = "FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
+          }),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+        }),
+
+        -- sources for autocompletion
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+
+        -- configure lspkind for vs-code like pictograms in completion menu
+        formatting = {
+          format = lspkind.cmp_format({
+            maxwidth = 50,
+            ellipsis_char = "...",
+          }),
+        },
+      })
+    end,
+  },
+}
--- home/bazz/.config/nvim/lua/plugins/ui.lua
+++ home/bazz/.config/nvim/lua/plugins/ui.lua
@@ -70,61 +70,8 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "VeryLazy",
     config = function()
-      local colors = {
-        green = "#66800B",
-        cyan = "#24837B",
-        black = "#100F0F",
-        white = "#CECDC3",
-        red = "#AF3029",
-        violet = "#A02F6F",
-        grey = "#403E3C",
-      }
-
-      local flexoki_theme = {
-        normal = {
-          a = { fg = colors.black, bg = colors.cyan },
-          b = { fg = colors.white, bg = colors.grey },
-          c = { fg = colors.white },
-        },
-
-        insert = { a = { fg = colors.black, bg = colors.green } },
-        visual = { a = { fg = colors.black, bg = colors.violet } },
-        replace = { a = { fg = colors.black, bg = colors.red } },
-
-        inactive = {
-          a = { fg = colors.white, bg = colors.black },
-          b = { fg = colors.white, bg = colors.black },
-          c = { fg = colors.white },
-        },
-      }
       require("lualine").setup({
-        options = {
-          theme = flexoki_theme,
-          component_separators = "",
-          section_separators = { left = "", right = "" },
-        },
-        sections = {
-          lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-          lualine_b = { "filename", "branch" },
-          lualine_c = {
-            "%=", --[[ add your center components here in place of this comment ]]
-          },
-          lualine_x = {},
-          lualine_y = { "filetype", "progress" },
-          lualine_z = {
-            { "location", separator = { right = "" }, left_padding = 2 },
-          },
-        },
-        inactive_sections = {
-          lualine_a = { "filename" },
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = { "location" },
-        },
-        tabline = {},
-        extensions = {},
+        options = { theme = "auto", section_separators = "", component_separators = "" },
       })
     end,
   },
