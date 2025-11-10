34363396486feab36947514e3cd7549ef70caa73
bitterhalt <bthalt@disroot.org>
2025-11-10 02:18:34 +0200

    Yes, I am idiot

--- home/bazz/.config/hypr/waybar/scripts/idle.sh
+++ home/bazz/.config/hypr/waybar/scripts/idle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if pgrep -x "hypridle" >/dev/null; then
-  echo "{\"text\": \"󰤄\", \"tooltip\": \"<b>Hypridle is enabled</b>\n\nClick left to disable\nClick right to Power-menu\",\"class\": \"enabled\"}"
+  echo "{\"text\": \"󰤄\", \"tooltip\": \"<b>Hypridle is enabled</b>\n\nClick right to disable\nClick left to Power-menu\",\"class\": \"enabled\"}"
 else
-  echo "{\"text\": \"󰠠\", \"tooltip\": \"<b>Hypridle is disable</b>\n\nClick left to enable\nClick right to Power-menu\", \"class\": \"disabled\"}"
+  echo "{\"text\": \"󰠠\", \"tooltip\": \"<b>Hypridle is disable</b>\n\nClick right to enable\nClick left to Power-menu\", \"class\": \"disabled\"}"
 fi
