4271bbf0bbd26fa450fd9c4c03900017faf9f124
bitterhalt <bthalt@disroot.org>
2025-12-05 00:15:30 +0200

    mostly ignis stuff

--- home/bazz/.local/bin/ignis-start.sh
+++ home/bazz/.local/bin/ignis-start.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/env bash
-
-if pgrep -x "ignis" >/dev/null; then
-  pkill ignis
-  sleep 1
-  ignis init >~/.local/state/ignis.log 2>&1 &
-else
-  ignis init >~/.local/state/ignis.log 2>&1 &
-fi
+/home/bazz/Documents/projects/ignis/scripts/ignis-start.sh
