ee0832d912eb9788bfc9e613e5651141a8d1b572
bitterhalt <bthalt@disroot.org>
2025-12-06 03:30:34 +0200

    add little helper for opening common projects

--- home/bazz/.config/hypr/scripts/dot-settings.sh
+++ home/bazz/.config/hypr/scripts/dot-settings.sh
@@ -5,24 +5,20 @@ ARCHIVE="BASH
 FNOTT
 FOOT
 FUZZEL
-HYPRIDLE
 HYPRLAND
-HYPRLOCK
+IGNIS
 NEOVIM
-WAYBAR
 "
 CHOICE=$(printf "$ARCHIVE" | fuzzel -d --minimal-lines -a top --y 4 -w 30 -p "" --placeholder "Edit settings: ")
-
 TERMCMD=("foot")
+OPENER=("vim_open")
 
 case $CHOICE in
-BASH) exec $TERMCMD -e nvim $HOME/.bashrc ;;
-FNOTT) exec $TERMCMD -e nvim $HOME/.config/fnott/fnott.ini ;;
-FOOT) exec $TERMCMD -e nvim $HOME/.config/foot/foot.ini ;;
-FUZZEL) exec $TERMCMD -e nvim $HOME/.config/fuzzel/fuzzel.ini ;;
-HYPRIDLE) exec $TERMCMD -e nvim $HOME/.config/hypr/hypridle.conf ;;
-HYPRLAND) exec $TERMCMD -e nvim $HOME/.config/hypr/modules/* $HOME/.config/hypr/colors.conf ;;
-HYPRLOCK) exec $TERMCMD -e nvim $HOME/.config/hypr/hyprlock.conf ;;
-NEOVIM) exec $TERMCMD -e nvim $HOME/.config/nvim/init.lua ;;
-WAYBAR) exec $TERMCMD -e nvim $HOME/.config/hypr/waybar/config.jsonc $HOME/.config/hypr/waybar/style.css ;;
+BASH) exec $TERMCMD -e nvim "$HOME"/.bashrc ;;
+FNOTT) exec $TERMCMD -e nvim "$HOME"/.config/fnott/fnott.ini ;;
+FOOT) exec $TERMCMD -e nvim "$HOME"/.config/foot/foot.ini ;;
+FUZZEL) exec $TERMCMD -e nvim "$HOME"/.config/fuzzel/fuzzel.ini ;;
+HYPRLAND) exec $TERMCMD -e $OPENER "$HOME"/.config/hypr/ ;;
+NEOVIM) exec $TERMCMD -e $OPENER "$HOME"/.config/nvim/ ;;
+IGNIS) exec $TERMCMD -e $OPENER "$HOME"/.config/ignis/ ;;
 esac
