36465a44953a8aa1d6c4c66f08d759019fba32f5
bitterhalt <bthalt@disroot.org>
2025-12-12 01:21:19 +0200

    fix typos, use goignis

--- home/bazz/.config/hypr/scripts/volume.sh
+++ home/bazz/.config/hypr/scripts/volume.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 case "${1:-}" in
 up)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
-  wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%+
+  wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%+ && goignis open-window ignis_VOLUME_OSD
   ;;
 down)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
-  wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%-
+  wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%- && goignis open-window ignis_VOLUME_OSD
   ;;
 mute)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
