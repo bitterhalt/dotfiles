7d089921efa99dac7264e80f420ebeb4b288b43f
bitterhalt <bthalt@disroot.org>
2025-12-30 00:24:11 +0200

    nothing special, mostly deleting useless stuff

--- home/bazz/.config/hypr/scripts/apps.sh
+++ home/bazz/.config/hypr/scripts/apps.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 $BROWSER &
+thunderbird &
 steam &
 transmission-gtk &
--- home/bazz/.config/hypr/scripts/workspace.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-workspace=$(hyprctl activeworkspace -j | jq -r '.name // .id')
-notify-send -t 1500 -h string:x-canonical-private-synchronous:notification "󰍹 Workspace: $workspace"
