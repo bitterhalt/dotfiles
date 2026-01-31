4caf3f117fac93c17e81dab03c941cd4fa43b5b8
bitterhalt <bthalt@disroot.org>
2026-01-31 13:50:42 +0200

    add some animations because cool kids use these :D

--- home/bazz/.config/hypr/scripts/hyprgamemode.sh
+++ home/bazz/.config/hypr/scripts/hyprgamemode.sh
@@ -8,10 +8,7 @@ if [ "$HYPRGAMEMODE" = 1 ]; then
         keyword animation borderangle,0; \
         keyword decoration:shadow:enabled 0;\
         keyword decoration:blur:enabled 0;\
-	      keyword decoration:fullscreen_opacity 1;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
-        keyword decoration:rounding 0"
+	      keyword decoration:fullscreen_opacity 1;"
   notify-send -t 5000 "Hyprland" "Fancy stuff disabled"
   exit 0
 else
