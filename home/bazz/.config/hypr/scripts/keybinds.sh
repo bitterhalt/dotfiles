5c28ecb88b8232dae04770ac766b5ffe8c4e94d8
bitterhalt <bthalt@disroot.org>
2026-02-04 01:55:29 +0200

    fix keybinds menu

--- home/bazz/.config/hypr/scripts/keybinds.sh
+++ home/bazz/.config/hypr/scripts/keybinds.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-grep 'bind =' ~/.config/hypr/modules/05-binds.conf | grep -v '^\s*#' | sed 's/^bind = //; s/, */ /g; s/\$[Mm]od/Super/g' | fuzzel -d -a top --y 4 -w 70 -l 25 -p "" --placeholder "Search keybinds..."
+grep 'bind =' ~/.config/hypr/modules/04-binds.conf | grep -v '^\s*#' | sed 's/^bind = //; s/, */ /g; s/\$[Mm]od/Super/g' | fuzzel -d -a top --y 4 -w 70 -l 25 -p "" --placeholder "Search keybinds..."
