899267b00c2068d965cc5fbcae71544ff622ce86
bitterhalt <bthalt@disroot.org>
2026-02-05 13:21:36 +0200

    add sound effect

--- home/bazz/.config/hypr/scripts/volume.sh
+++ home/bazz/.config/hypr/scripts/volume.sh
@@ -2,14 +2,24 @@
 
 set -euo pipefail
 
+SOUND="$HOME/.local/share/Sounds/audio-volume-change.oga"
+
+play_sound() {
+  [ -f "$SOUND" ] && pw-play "$SOUND" &
+}
+
 case "${1:-}" in
 up)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
-  wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%+ && goignis open-window ignis_VOLUME_OSD
+  wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%+
+  play_sound
+  goignis open-window ignis_VOLUME_OSD
   ;;
 down)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
-  wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%- && goignis open-window ignis_VOLUME_OSD
+  wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%-
+  play_sound
+  goignis open-window ignis_VOLUME_OSD
   ;;
 mute)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
