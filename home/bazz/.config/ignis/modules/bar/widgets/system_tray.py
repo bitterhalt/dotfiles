d0d3ba335eb3caea1975ab8e73576aeb522976ed
bitterhalt <bthalt@disroot.org>
2026-02-06 00:20:07 +0200

    ignis: hide ugly tray icons under expander

--- home/bazz/.config/ignis/modules/bar/widgets/system_tray.py
+++ home/bazz/.config/ignis/modules/bar/widgets/system_tray.py
@@ -30,12 +30,70 @@ class TrayItem(widgets.Button):
 
 class SystemTrayWidget(widgets.Box):
     def __init__(self):
+        self._expanded = False
+
+        self._arrow = widgets.Icon(
+            image="pan-start-symbolic",
+            pixel_size=16,
+            css_classes=["tray-arrow"],
+        )
+
+        self._toggle_btn = widgets.Button(
+            css_classes=["tray-toggle-btn", "unset"],
+            child=self._arrow,
+            on_click=lambda x: self._toggle_tray(),
+            tooltip_text="Show system tray",
+        )
+
+        self._items_box = widgets.Box(
+            css_classes=["tray-items"],
+        )
+
+        self._revealer = widgets.Revealer(
+            child=self._items_box,
+            reveal_child=False,
+            transition_type="slide_left",
+            transition_duration=200,
+        )
+
         super().__init__(
             css_classes=["system-tray"],
-            spacing=12,
+            child=[self._toggle_btn, self._revealer],
         )
 
         for item in system_tray.items:
-            self.append(TrayItem(item))
+            self._items_box.append(TrayItem(item))
 
-        system_tray.connect("added", lambda x, item: self.append(TrayItem(item)))
+        system_tray.connect("added", lambda x, item: self._on_item_added(item))
+
+        self._update_visibility()
+
+    def _on_item_added(self, item):
+        """Handle new tray item"""
+        tray_item = TrayItem(item)
+        self._items_box.append(tray_item)
+
+        item.connect("removed", lambda x: self._update_visibility())
+
+        self._update_visibility()
+
+    def _update_visibility(self):
+        has_items = len(self._items_box.child) > 0
+        self.visible = has_items
+
+        if not has_items and self._expanded:
+            self._expanded = False
+            self._revealer.reveal_child = False
+            self._arrow.image = "pan-start-symbolic"
+            self._toggle_btn.set_tooltip_text("Show system tray")
+
+    def _toggle_tray(self):
+        self._expanded = not self._expanded
+        self._revealer.reveal_child = self._expanded
+
+        if self._expanded:
+            self._arrow.image = "pan-end-symbolic"
+            self._toggle_btn.set_tooltip_text("Hide system tray")
+        else:
+            self._arrow.image = "pan-start-symbolic"
+            self._toggle_btn.set_tooltip_text("Show system tray")
