687147d7d3a6a7a0ef7c340a5da1cd4090271b15
bitterhalt <bthalt@disroot.org>
2026-03-06 12:30:15 +0200

    nada

--- home/bazz/.config/hypr/scripts/volume.sh
+++ home/bazz/.config/hypr/scripts/volume.sh
@@ -13,13 +13,13 @@ case "${1:-}" in
 up)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
   wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%+
-  play_sound
+  # play_sound
   $NOTIFY
   ;;
 down)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
   wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%-
-  play_sound
+  # play_sound
   $NOTIFY
   ;;
 mute)
