267947b1a7bb379c744fad21c6802d0909a235a7
bitterhalt <bthalt@disroot.org>
2026-03-11 11:35:40 +0200

    add effects back since pipewire fixed their shit..

--- home/bazz/.config/hypr/scripts/mic-toggle.sh
+++ home/bazz/.config/hypr/scripts/mic-toggle.sh
@@ -4,14 +4,10 @@ sounds="$HOME/.local/share/Sounds"
 
 set -euo pipefail
 
-# Toggle mute state
 wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
 
-# Check the current state
 if wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -qi 'MUTED'; then
-  # Mic is muted
-  # pw-play "$sounds"/device-removed.oga &
+  pw-play "$sounds"/device-removed.oga &
 else
-  # Mic is unmuted
-  # pw-play "$sounds"/audio-volume-change.oga &
+  pw-play "$sounds"/audio-volume-change.oga &
 fi
