1867d34facf29cc31e828fe6091fd4a9a1083db9
bitterhalt <bthalt@disroot.org>
2026-03-22 15:31:39 +0200

    ignis: media_osd now updates in real time

--- home/bazz/.config/ignis/modules/osd/media_osd.py
+++ home/bazz/.config/ignis/modules/osd/media_osd.py
@@ -1,5 +1,5 @@
 from ignis import utils, widgets
-from ignis.services.mpris import MprisService
+from ignis.services.mpris import MprisService, MprisPlayer
 from modules.utils.signal_manager import SignalManager
 from modules.utils.media_utils import MediaPlayerInfo, MediaPlayerControls, MediaPlayerConfig
 from settings import config
@@ -12,7 +12,8 @@ class MediaOsdWindow(widgets.RevealerWindow):
     def __init__(self):
         self._timeout = None
         self._bound_player = None
-        self._signals = SignalManager()
+        self._player_signals = SignalManager()
+        self._mpris_signals = SignalManager()
 
         self._app_icon = widgets.Icon(
             image=MediaPlayerConfig.PLAYER_ICONS[None],
@@ -133,65 +134,59 @@ class MediaOsdWindow(widgets.RevealerWindow):
             revealer=revealer,
         )
 
+        self._mpris_signals.connect(mpris, "notify::players", self._on_players_changed)
         self.connect("notify::visible", self._on_visible_changed)
         self.connect("destroy", self._cleanup)
+        self._bind_active_player()
 
     def _cleanup(self, *_):
-        self._signals.disconnect_all()
+        self._player_signals.disconnect_all()
+        self._mpris_signals.disconnect_all()
         if self._timeout:
             try:
                 self._timeout.cancel()
-            except:
+            except Exception:
                 pass
             self._timeout = None
 
-    def _on_visible_changed(self, *_):
-        if self.get_visible():
-            self._update_content()
-
-            if self._timeout:
-                self._timeout.cancel()
-
-            self._timeout = utils.Timeout(
-                TIMEOUT,
-                lambda: self.set_visible(False),
-            )
-        else:
-            if self._timeout:
-                self._timeout.cancel()
-                self._timeout = None
-
-    def show_osd(self):
-        self.set_visible(True)
-
-    def _bind_play_icon(self, player):
-        self._signals.disconnect_all()
-
-        def update_icon(*_):
-            self._btn_play_icon.image = MediaPlayerControls.get_play_pause_icon(player)
-
-        self._signals.connect(player, "notify::playback-status", update_icon)
-        update_icon()
-
-    def _update_content(self):
+    def _on_players_changed(self, *_):
         player = MediaPlayerControls.get_active_player()
+        if player != self._bound_player:
+            self._bind_active_player()
+
+    def _bind_active_player(self):
+        self._player_signals.disconnect_all()
+        player = MediaPlayerControls.get_active_player()
+        self._bound_player = player
 
         if not player:
-            self._bound_player = None
-            self._app_icon.image = MediaPlayerConfig.PLAYER_ICONS[None]
-            self._app_name.label = ""
-            self._title_label.label = ""
-            self._artist_label.label = "No media playing"
-            self._album_art.visible = False
-            self._btn_prev.visible = False
-            self._btn_play.visible = False
-            self._btn_next.visible = False
+            self._apply_no_player()
             return
 
-        self._btn_prev.visible = True
-        self._btn_play.visible = True
-        self._btn_next.visible = True
+        self._apply_player(player)
 
+        self._player_signals.connect(player, "notify::title", lambda *_: self._on_track_changed(player))
+        self._player_signals.connect(player, "notify::artist", lambda *_: self._on_track_changed(player))
+        self._player_signals.connect(player, "notify::art-url", lambda *_: self._on_track_changed(player))
+        self._player_signals.connect(player, "notify::playback-status", lambda *_: self._update_play_icon(player))
+        self._player_signals.connect(player, "notify::can-go-previous", lambda *_: self._update_button_states(player))
+        self._player_signals.connect(player, "notify::can-go-next", lambda *_: self._update_button_states(player))
+
+    def _on_track_changed(self, player: MprisPlayer):
+        self._apply_player(player)
+        self.show_osd()
+
+    def _apply_no_player(self):
+        self._app_icon.image = MediaPlayerConfig.PLAYER_ICONS[None]
+        self._app_name.label = ""
+        self._title_label.label = ""
+        self._artist_label.label = "No media playing"
+        self._album_art.visible = False
+        self._btn_prev.visible = False
+        self._btn_play.visible = False
+        self._btn_next.visible = False
+
+    def _apply_player(self, player: MprisPlayer):
         self._app_icon.image = MediaPlayerInfo.get_player_icon(player)
         self._app_name.label = MediaPlayerInfo.get_player_name(player)
         self._title_label.label = player.title or "Unknown Title"
@@ -203,9 +198,32 @@ class MediaOsdWindow(widgets.RevealerWindow):
         else:
             self._album_art.visible = False
 
-        if self._bound_player != player:
-            self._bind_play_icon(player)
-            self._bound_player = player
+        self._btn_prev.visible = True
+        self._btn_play.visible = True
+        self._btn_next.visible = True
 
+        self._update_play_icon(player)
+        self._update_button_states(player)
+
+    def _update_play_icon(self, player: MprisPlayer):
+        self._btn_play_icon.image = MediaPlayerControls.get_play_pause_icon(player)
+
+    def _update_button_states(self, player: MprisPlayer):
         self._btn_prev.set_sensitive(player.can_go_previous)
         self._btn_next.set_sensitive(player.can_go_next)
+
+    def _on_visible_changed(self, *_):
+        if self.get_visible():
+            if self._timeout:
+                self._timeout.cancel()
+            self._timeout = utils.Timeout(
+                TIMEOUT,
+                lambda: self.set_visible(False),
+            )
+        else:
+            if self._timeout:
+                self._timeout.cancel()
+                self._timeout = None
+
+    def show_osd(self):
+        self.set_visible(True)
