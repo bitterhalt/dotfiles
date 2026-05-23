506f8fd199019a81841e8ac7aee1deb5b17661bb
bitterhalt <bthalt@disroot.org>
2026-05-23 09:11:37 +0300

    edit mako + add script

---
+++ home/bazz/.config/niri/scripts/mediactrl.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+notify_track() {
+  if playerctl status == "Playing" >/dev/null; then
+    title=$(playerctl metadata title 2>/dev/null || echo "Unknown Title")
+    artist=$(playerctl metadata artist 2>/dev/null || echo "")
+    notify-send -t 5000 -i video-x-generic "$artist" "$title" -h string:x-canonical-private-synchronous:volume
+  fi
+}
+
+play_next() {
+  playerctl next
+}
+
+play_previous() {
+  playerctl previous
+}
+
+toggle_play_pause() {
+  playerctl play-pause
+}
+
+stop_playback() {
+  playerctl stop
+}
+
+case "$1" in
+"--nxt")
+  play_next
+  ;;
+"--prv")
+  play_previous
+  ;;
+"--play")
+  toggle_play_pause
+  ;;
+"--stop")
+  stop_playback
+  ;;
+"--noti")
+  notify_track
+  ;;
+*)
+  echo "Usage: $0 [--nxt|--prv|--play|--stop|--noti]"
+  exit 1
+  ;;
+esac
