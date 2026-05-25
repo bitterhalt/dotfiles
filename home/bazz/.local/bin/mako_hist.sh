25d7aec756883b6bfea152bbfd41794e0b16591d
bitterhalt <bthalt@disroot.org>
2026-05-25 20:36:17 +0300

    more readable

--- home/bazz/.local/bin/mako_hist.sh
+++ home/bazz/.local/bin/mako_hist.sh
@@ -1,2 +1,21 @@
-#!/usr/bin/env bash
-makoctl history -j | jq -r ".[] | \"\(if .urgency == \"critical\" then \" ❗ \" else \" 🔔 \" end)[\(.app_name)] \(.summary) ➜ \(.body)\"" | fzf --no-info --layout reverse --border-label="Mako-history" --delimiter " ➜ " --preview "echo {2} | fmt -s" --preview-window=bottom:wrap
+makoctl history -j |
+  jq -r '
+      .[] | (
+        if .urgency == "critical"
+        then " ❗ "
+        else " 🔔 "
+        end
+      ) + "[\(.app_name)] \(.summary)\(
+        if (.body // "") != ""
+        then " ➜ \(.body)"
+        else ""
+        end
+      )"
+    ' |
+  fzf \
+    --no-info \
+    --layout reverse \
+    --border-label "Mako-history" \
+    --delimiter " ➜ " \
+    --preview "echo {2} | fmt -s" \
+    --preview-window bottom:wrap
