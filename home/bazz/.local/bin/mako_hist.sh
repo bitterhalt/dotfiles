aebefb32f237c0f3246639951fe8c476142ae513
bitterhalt <bthalt@disroot.org>
2026-05-25 18:13:26 +0300

    mako_hist: add colored indicators

--- home/bazz/.local/bin/mako_hist.sh
+++ home/bazz/.local/bin/mako_hist.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-
-makoctl history -j | jq -r ".[] | \"[\(.app_name)] \(.summary) ➜ \(.body)\"" | fzf --layout reverse --border-label="Mako-history" --delimiter " ➜ " --preview "echo {2} | fmt -s" --preview-window=bottom:wrap
+makoctl history -j | jq -r ".[] | \"\(if .urgency == \"critical\" then \" ❗ \" else \" 🔔 \" end)[\(.app_name)] \(.summary) ➜ \(.body)\"" | fzf --no-info --layout reverse --border-label="Mako-history" --delimiter " ➜ " --preview "echo {2} | fmt -s" --preview-window=bottom:wrap
