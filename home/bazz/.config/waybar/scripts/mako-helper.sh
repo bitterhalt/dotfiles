2fab20d8c8912132dd668b1087c0eae3343c1163
bitterhalt <bthalt@disroot.org>
2026-05-26 21:13:44 +0300

    if 0, don't display count

--- home/bazz/.config/waybar/scripts/mako-helper.sh
+++ home/bazz/.config/waybar/scripts/mako-helper.sh
@@ -2,10 +2,16 @@
 
 COUNT=$(makoctl history -j | jq '. | length')
 
+if [ "$COUNT" -eq 0 ]; then
+  DISPLAY_COUNT=""
+else
+  DISPLAY_COUNT="$COUNT"
+fi
+
 if makoctl mode | grep -q 'dnd'; then
   MODE="dnd"
 else
   MODE="default"
 fi
 
-printf '{"text": " %s", "alt": "%s"}\n' "$COUNT" "$MODE"
+printf '{"text": " %s", "alt": "%s"}\n' "$DISPLAY_COUNT" "$MODE"
