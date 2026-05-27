88afbb3fa64992ba4e5ca99536a4ce580aa4a7b8
bitterhalt <bthalt@disroot.org>
2026-05-27 17:07:33 +0300

    add "osd" for volume

--- home/bazz/.config/niri/scripts/volume.sh
+++ home/bazz/.config/niri/scripts/volume.sh
@@ -2,27 +2,42 @@
 
 set -euo pipefail
 
-SOUND="$HOME/.local/share/Sounds/audio-volume-change.oga"
-
-play_sound() {
-  [ -f "$SOUND" ] && pw-play "$SOUND" &
-}
+noti="notify-send -t 1000 -a vol-osd -h string:x-canonical-private-synchronous:volume"
 
 case "${1:-}" in
 up)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
   wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%+
-  play_sound
   ;;
 down)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
   wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%-
-  play_sound
   ;;
 mute)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
   ;;
-mutemic)
-  wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-  ;;
 esac
+
+vol="$(wpctl get-volume @DEFAULT_AUDIO_SINK@)"
+
+[ "$vol" != "${vol%\[MUTED\]}" ] && $noti "Muted" && exit
+
+vol="${vol#Volume: }"
+
+split() {
+  IFS=$2
+  set -- $1
+  printf '%s' "$@"
+}
+
+vol="$(printf "%.0f" "$(split "$vol" ".")")"
+
+case 1 in
+$((vol >= 1))) ;;
+*) $noti "Muted" && exit ;;
+esac
+
+$noti \
+  "$vol%" \
+  -h int:value:"${vol}" \
+  -h string:x-canonical-private-synchronous:volume
