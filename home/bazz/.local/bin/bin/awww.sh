62e505d771d1dea17e51426f94784dfed86000ca
bitterhalt <bthalt@disroot.org>
2026-06-24 17:07:57 +0300

    whoooooooops

--- home/bazz/.local/bin/bin/awww.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-pgrep -x awww-daemon >/dev/null && pkill awww-daemon
-
-awww-daemon &
-awww-daemon -n overview &
--- home/bazz/.local/bin/bin/bar-hider.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if pgrep waybar >/dev/null; then
-  pkill waybar
-else
-  waybar &
-fi
