f6bdc5f5c92582765655e4f4be4a8e99f77a4a60
bitterhalt <bthalt@disroot.org>
2026-07-14 11:03:24 +0300

    align better for human eyes

--- home/bazz/.config/niri/scripts/keybinds.sh
+++ home/bazz/.config/niri/scripts/keybinds.sh
@@ -2,7 +2,7 @@
 
 CONFIG_FILE="$HOME/.config/niri/binds.kdl"
 
-grep "hotkey-overlay-title=" "$CONFIG_FILE" |
-  sed -E 's/^[[:space:]]*//;
-          s/([[:alnum:]+]+) hotkey-overlay-title="([^"]+)".*/\1  ➜  \2/' |
+grep 'hotkey-overlay-title=' "$CONFIG_FILE" |
+  sed -En 's/^[[:space:]]*([[:alnum:]+]+).*hotkey-overlay-title="([^"]+)".*/\1\t\2/p' |
+  awk -F'\t' '{ printf "%-18s → %s\n", $1, $2 }' |
   fuzzel -d -a top --y 8 -w 50 -l 15 -p "" --placeholder "Search keybinds"
