a898ee005875244a6354c08dc7f1a7b9cd05d5ed
bitterhalt <bthalt@disroot.org>
2026-08-29 15:40:20 +0300

    make tooltip do something useful

--- home/bazz/.config/waybar/scripts/mako-helper.sh
+++ home/bazz/.config/waybar/scripts/mako-helper.sh
@@ -5,6 +5,7 @@ COUNT=$(makoctl history -j | jq '. | length')
 if makoctl mode | grep -q 'dnd'; then
   MODE="dnd"
   DISPLAY_TEXT="󰂛"
+  TOOLTIP="Do Not Disturb"
 else
   MODE="default"
   if [ "$COUNT" -gt 0 ]; then
@@ -12,6 +13,7 @@ else
   else
     DISPLAY_TEXT=""
   fi
+  TOOLTIP="You have $COUNT notifications"
 fi
 
-printf '{"text": "%s", "class": "%s"}\n' "$DISPLAY_TEXT" "$MODE"
+printf '{"text": "%s", "class": "%s", "tooltip": "%s"}\n' "$DISPLAY_TEXT" "$MODE" "$TOOLTIP"
