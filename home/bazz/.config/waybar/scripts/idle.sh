2f1359c8779adc630b5eb02ae99ceb9503babfd6
bitterhalt <bthalt@disroot.org>
2026-09-22 12:22:02 +0300

    mv group for networking stuff

--- home/bazz/.config/waybar/scripts/idle.sh
+++ home/bazz/.config/waybar/scripts/idle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if pgrep -x "swayidle" >/dev/null; then
-  echo "{\"text\": \"󰤄\", \"tooltip\": \"Idle daemon is enabled\", \"class\": \"enabled\"}"
+  echo ""
 else
   echo "{\"text\": \"󱐋\", \"tooltip\": \"Idle daemon is disabled\", \"class\": \"disabled\"}"
 fi
